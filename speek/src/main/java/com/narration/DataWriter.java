89c4d01c0826651fd861d874289509ee95e3995e
joshnatkin <jnatkin@email.sc.edu>
2024-10-25 16:22:10 -0400

    saveUsers

--- speek/src/main/java/com/narration/DataWriter.java
+++ speek/src/main/java/com/narration/DataWriter.java
@@ -1,4 +1,5 @@
 package com.narration;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,61 +19,83 @@ public class DataWriter {
 
 
     //done3
-    public boolean saveUsers(ArrayList<User> users) {
-        JSONArray userArray = new JSONArray();
+    public boolean saveUsers(ArrayList<User> newUsers) {
+    JSONArray userArray = new JSONArray();
 
-        for (User user : users) {
-            JSONObject userJson = new JSONObject();
-            userJson.put("id", user.getId().toString());
-            userJson.put("username", user.getUsername());
-            userJson.put("email", user.getEmail());
-            userJson.put("password", user.getPassword());
-
-            JSONArray coursesJson = new JSONArray();
-            for (Course course : user.getCourses()) {
-                JSONObject courseJson = new JSONObject();
-                courseJson.put("courseID", course.getId().toString());
-                courseJson.put("courseProgress", course.getCourseProgress());
-                coursesJson.add(courseJson);
-            }
-            userJson.put("courses", coursesJson);
-
-            JSONObject progressJson = new JSONObject();
-            HashMap<UUID, Double> progress = user.getProgress();  // Parameterized HashMap
-            for (UUID courseId : user.getProgress().keySet()) {
-                progressJson.put(courseId.toString(), user.getProgress().get(courseId));
-            }
-            userJson.put("progress", progressJson);
-
-            JSONArray completedCoursesJson = new JSONArray();
-            for (UUID completedCourseId : user.getCompletedCourses()) {
-                completedCoursesJson.add(completedCourseId.toString());
-            }
-            userJson.put("completedCourses", completedCoursesJson);
-
-            JSONObject currentCourseJson = new JSONObject();
-            currentCourseJson.put("courseID", user.getCurrentCourse().toString());
-            userJson.put("currentCourse", currentCourseJson);
-
-            JSONArray languagesJson = new JSONArray();
-            for (Language language : user.getLanguages()) {
-                JSONObject languageJson = new JSONObject();
-                languageJson.put("languageID", language.getId().toString());
-                languageJson.put("name", language.getName());
-                languagesJson.add(languageJson);
-            }
-            userJson.put("languages", languagesJson);
-
-            JSONObject currentLanguageJson = new JSONObject();
-            currentLanguageJson.put("languageID", user.getCurrentLanguage().toString());
-            currentLanguageJson.put("name", user.getCurrentLanguageName());
-            userJson.put("currentLanguage", currentLanguageJson);
-
-            userArray.add(userJson);
-        }
-
-        return writeToFile(USERS_FILE, userArray);
+    // Read existing users from the file
+    try (FileReader reader = new FileReader(USERS_FILE)) {
+        JSONArray existingUsers = (JSONArray) new org.json.simple.parser.JSONParser().parse(reader);
+        userArray.addAll(existingUsers);  // Append existing users
+    } catch (Exception e) {
+        e.printStackTrace();  // Handle exceptions for file reading/parsing
     }
+
+    // Add new users to the userArray
+    for (User user : newUsers) {
+        JSONObject userJson = new JSONObject();
+        userJson.put("id", user.getId().toString());
+        userJson.put("username", user.getUsername());
+        userJson.put("email", user.getEmail());
+        userJson.put("password", user.getPassword());
+
+        JSONArray coursesJson = new JSONArray();
+        for (Course course : user.getCourses()) {
+            JSONObject courseJson = new JSONObject();
+            courseJson.put("courseID", course.getId().toString());
+            courseJson.put("courseProgress", course.getCourseProgress());
+            coursesJson.add(courseJson);
+        }
+        userJson.put("courses", coursesJson);
+
+        JSONObject progressJson = new JSONObject();
+        HashMap<UUID, Double> progress = user.getProgress();  // Assuming you have this method
+        for (UUID courseId : progress.keySet()) {
+            progressJson.put(courseId.toString(), progress.get(courseId));
+        }
+        userJson.put("progress", progressJson);
+
+        JSONArray completedCoursesJson = new JSONArray();
+        for (UUID completedCourseId : user.getCompletedCourses()) {
+            completedCoursesJson.add(completedCourseId.toString());
+        }
+        userJson.put("completedCourses", completedCoursesJson);
+
+        JSONObject currentCourseJson = new JSONObject();
+        currentCourseJson.put("courseID", user.getCurrentCourse().toString());
+        userJson.put("currentCourse", currentCourseJson);
+
+        JSONArray languagesJson = new JSONArray();
+        for (Language language : user.getLanguages()) {
+            JSONObject languageJson = new JSONObject();
+            languageJson.put("languageID", language.getId().toString());
+            languageJson.put("name", language.getName());
+            languagesJson.add(languageJson);
+        }
+        userJson.put("languages", languagesJson);
+
+        JSONObject currentLanguageJson = new JSONObject();
+        currentLanguageJson.put("languageID", user.getCurrentLanguage().toString());
+        currentLanguageJson.put("name", user.getCurrentLanguageName());
+        userJson.put("currentLanguage", currentLanguageJson);
+
+        userArray.add(userJson);
+    }
+
+    // Write the updated userArray back to the file
+    return writeToFile(USERS_FILE, userArray);
+}
+
+// Helper method to write to the file
+private boolean writeToFile(String filePath, JSONArray jsonArray) {
+    try (FileWriter file = new FileWriter(filePath)) {
+        file.write(jsonArray.toJSONString());
+        file.flush();
+        return true;
+    } catch (IOException e) {
+        e.printStackTrace();  // Handle exceptions for file writing
+        return false;
+    }
+}
 
     //done
     public boolean saveCourses(ArrayList<Course> courses) {
@@ -148,18 +171,6 @@ public class DataWriter {
             }
         }
         saveUsers(users);
-    }
-
-    //done
-    private boolean writeToFile(String filePath, JSONArray jsonArray) {
-        try (FileWriter file = new FileWriter(filePath)) {
-            file.write(jsonArray.toJSONString());
-            file.flush();
-            return true;
-        } catch (IOException e) {
-            System.err.println("Error writing to file: " + e.getMessage());
-            return false;
-        }
     }
 
     // Save words to the JSON file
